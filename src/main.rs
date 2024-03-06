5d73c0038459855b1d855d4f64c061bbb5560f2c
lmw9288 <lmw9288@gmail.com>
2024-03-06 23:40:24 +0800

    feat(user): /api/users/login

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
+
+use rand::prelude::*;
+
 use serde::{Deserialize, Serialize};
 
 #[tokio::main]
@@ -13,7 +16,9 @@ async fn main() {
         .route(
             "/api/users",
             post(|Json(UserRequest { user }): Json<UserRequest>| async {
-                println!("{:?}", user);
+                println!("POST /api/users, body: {:?}", user);
+                // let mut rng = rand::thread_rng();
+                // let token: usize = rng.gen();
                 let user_response = UserResponse {
                     user: UserResponseModel {
                         username: user.username,
@@ -25,11 +30,40 @@ async fn main() {
                 };
                 Json(user_response)
             }),
+        )
+        .route(
+            "/api/users/login",
+            post(
+                |Json(UserLoginRequest { user }): Json<UserLoginRequest>| async {
+                    println!("POST /api/users/login, body: {:?}", user);
+                    let user_response = UserResponse {
+                        user: UserResponseModel {
+                            username: "user.username".to_owned(),
+                            email: user.email,
+                            token: "test".to_owned(),
+                            bio: "test".to_owned(),
+                            image: None,
+                        },
+                    };
+                    Json(user_response)
+                },
+            ),
         );
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+#[derive(Deserialize, Debug)]
+struct UserLoginRequest {
+    user: UserLoginModel,
+}
+
+#[derive(Deserialize, Debug)]
+struct UserLoginModel {
+    email: String,
+    password: String,
 }
 
 #[derive(Deserialize)]
